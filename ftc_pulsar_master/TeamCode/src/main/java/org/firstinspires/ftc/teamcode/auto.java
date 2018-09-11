accbb30c270014f8bb462584cf5eea175465ef9a
LucaSandoval <lucasandoval@seattleacademy.org>
2018-09-11 16:36:22 -0700

    Rip the auto

--- ftc_pulsar_master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto.java
+++ ftc_pulsar_master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto.java
@@ -9,35 +9,4 @@ public class auto {
 
 
 
-    public void drive(float time, float power){ // basic drive command
-        hardware.front_left_motor.setPower(power);
-        hardware.back_left_motor.setPower(power);
-        hardware.front_right_motor.setPower(power);
-        hardware.back_right_motor.setPower(power);
-
-    }
-
-
-    public void turn(int direction, float time, float power){
-
-        if (direction == 1){ // left
-
-         hardware.front_left_motor.setPower(power);
-         hardware.back_left_motor.setPower(power);
-
-        } else if (direction == 2){ // right
-
-         hardware.front_right_motor.setPower(power);
-         hardware.back_right_motor.setPower(power);
-
-        }
-    }
-
-
-
-
-
-
-
-
 }

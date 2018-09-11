19d823afcfc1db0810c39b883a56e2589256a0b5
LucaSandoval <lucasandoval@seattleacademy.org>
2018-09-11 16:44:02 -0700

    set up the auto framework

--- ftc_pulsar_master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto.java
+++ ftc_pulsar_master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto.java
@@ -2,10 +2,29 @@ package org.firstinspires.ftc.teamcode;
 
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 
 @Autonomous(name = "Auto")
-public class auto {
+public class auto extends LinearOpMode{
+
+    // dont even need to refrence motors lol
+
+
+    @Override
+    public void runOpMode(){
+
+        // dont need anything here either lol
+
+        waitForStart();
+        while (opModeIsActive()){
+
+            // the auto instructions list from robot.java
+
+
+        }
+
+    }
 
 
 

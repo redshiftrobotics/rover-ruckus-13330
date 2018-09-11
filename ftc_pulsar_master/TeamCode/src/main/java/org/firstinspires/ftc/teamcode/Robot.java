4184ea5ca746d72d0e1360f58b5d1b909411d5d1
Julian <julian@allchin.com>
2018-09-11 16:40:28 -0700

    Robot Class changes

--- ftc_pulsar_master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
+++ ftc_pulsar_master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
@@ -2,13 +2,17 @@ package org.firstinspires.ftc.teamcode;
 
 
 public class Robot {
-
-
     class LeftDrive {
-
+        void setPower(double input){
+            hardware.back_left_motor.setPower(input);
+            hardware.front_left_motor.setPower(input);
+        }
     }
 
     class RightDrive {
-        
+        void setPower(double input) {
+            hardware.back_right_motor.setPower(input);
+            hardware.front_right_motor.setPower(input);
+        }
     }
 }

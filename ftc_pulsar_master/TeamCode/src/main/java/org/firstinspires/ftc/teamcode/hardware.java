b8567ecab47c895684a45fb901ab763a84f1465a
LucaSandoval <lucasandoval@seattleacademy.org>
2018-09-12 15:37:58 -0700

    Commenting

--- ftc_pulsar_master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware.java
+++ ftc_pulsar_master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware.java
@@ -4,14 +4,16 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 
 
-abstract class hardware extends OpMode{
+abstract class hardware extends OpMode{ // Here we get the DcMotors from the REV hub and assign their names.
+
     public static DcMotor front_right_motor;
     public static DcMotor front_left_motor;
     public static DcMotor back_right_motor;
     public static DcMotor back_left_motor;
 
     @Override
-    public void init() {
+    public void init() { // here we assign the motors to names in the hardware map.
+
         hardware.front_left_motor = hardwareMap.dcMotor.get("name");
         hardware.front_right_motor = hardwareMap.dcMotor.get("name");
         hardware.back_left_motor = hardwareMap.dcMotor.get("name");

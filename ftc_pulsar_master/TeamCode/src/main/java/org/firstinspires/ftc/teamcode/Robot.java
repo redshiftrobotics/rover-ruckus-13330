7c0aee38d583638d8e5843d7becc485b59cafb32
Julian <julian@allchin.com>
2018-09-12 15:37:57 -0700

    Adding the getcurrentpos

--- ftc_pulsar_master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
+++ ftc_pulsar_master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
@@ -23,12 +23,26 @@ public class Robot {
                 hardware.back_left_motor.setPower(power);
                 hardware.front_left_motor.setPower(power);
             }
+            public static double[] getCurrentPosition(){
+                double[] currentPos = new double[2];
+                currentPos[0] = hardware.front_left_motor.getCurrentPosition();
+                currentPos[1] = hardware.back_left_motor.getCurrentPosition();
+
+                return currentPos;
+            }
         }
 
         public static class RightDrive {
             public static void setPower(double input) {
                 hardware.back_right_motor.setPower(input);
                 hardware.front_right_motor.setPower(input);
+            }
+            public static double[] getCurrentPosition(){
+                double[] currentPos = new double[2];
+                currentPos[0] = hardware.front_right_motor.getCurrentPosition();
+                currentPos[1] = hardware.back_right_motor.getCurrentPosition();
+
+                return currentPos;
             }
         }
     }

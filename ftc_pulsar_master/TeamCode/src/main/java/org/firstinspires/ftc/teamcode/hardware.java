111795cecc192aadacfbfe7ccdec5aaedc05e584
Julian <julian@allchin.com>
2018-09-12 16:59:13 -0700

    Fixing my previous fixes to 'fixes'

--- ftc_pulsar_master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware.java
+++ ftc_pulsar_master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware.java
@@ -6,12 +6,14 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 
 abstract class hardware extends OpMode{ // Here we get the DcMotors from the REV hub and assign their names.
 
+    public static DcMotor front_right_motor;
+    public static DcMotor front_left_motor;
+    public static DcMotor back_right_motor;
+    public static DcMotor back_left_motor;
+
     @Override
     public void init() { // here we assign the motors to names in the hardware map.
-        DcMotor front_right_motor;
-        DcMotor front_left_motor;
-        DcMotor back_right_motor;
-        DcMotor back_left_motor;
+
 
         front_left_motor = hardwareMap.dcMotor.get("front_left_motor");
         front_right_motor = hardwareMap.dcMotor.get("front_right_motor");

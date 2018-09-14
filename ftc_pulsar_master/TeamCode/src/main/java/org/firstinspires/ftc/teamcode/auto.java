82274ca5e05183417b421f9d8fd6782ac5293d76
LucaSandoval <lucasandoval@seattleacademy.org>
2018-09-13 17:38:40 -0700

    Un-Hecked Auto Test

--- ftc_pulsar_master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto.java
+++ ftc_pulsar_master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto.java
@@ -1,35 +1,39 @@
 package org.firstinspires.ftc.teamcode;
 
-
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.eventloop.opmode.OpMode;
-import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
 
 
 @Autonomous(name = "Auto")
-public class auto extends OpMode { // comments written by Mr. Luca Sandoval
+public class auto extends LinearOpMode { // comments written by Mr. Luca Sandoval
 
-    public String versionName = "BETA v0.01";
+    public String versionName = "BETA v0.01"; // version name for organization.
 
     private Hardware hardware;
     private Robot robot;
 
     @Override
-    public void init(){
+    public void runOpMode(){ // the method that runs everything.
         this.hardware = new Hardware(this);
         this.robot = new Robot(this.hardware);
 
         telemetry.addData("Initialized Auto Version: ", versionName);
-    }
+
+        waitForStart();
 
 
-    @Override
-    public void loop() {
-        robot.setPowerLeft(1);
+        robot.setPowerLeft(1); // goes forward for one second.
+        robot.setPowerRight(1);
+        sleep(1000);
+        robot.setPowerLeft(1); //goes right for one second.
+        robot.setPowerRight(0.5);
+        sleep(1000);
+
 
         telemetry.addData("Auto Completed.", "");
         telemetry.update();
 
     }
-
+    
 }

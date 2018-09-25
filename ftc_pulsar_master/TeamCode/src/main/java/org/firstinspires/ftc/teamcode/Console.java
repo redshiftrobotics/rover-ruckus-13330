d3c43b5f991a8f15291664e7db39e7050f407de9
LucaSandoval <lucasandoval@seattleacademy.org>
2018-09-25 16:28:08 -0700

    Changes 'ctx' to 'context' in Console.java

--- ftc_pulsar_master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Console.java
+++ ftc_pulsar_master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Console.java
@@ -5,34 +5,34 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 public class Console { // parent class
 
 
-    private Hardware hardware; // get the ctx of other classes to use
+    private Hardware hardware; // get the context of other classes to use
     private Robot robot;
 
-    public Console(Hardware hardware, Robot robot) { // creates the ctx in this class
+    public Console(Hardware hardware, Robot robot) { // creates the context in this class
         this.hardware = hardware;
         this.robot = robot;
     }
 
-    public void displayAngles(LinearOpMode ctx){ // used to display the current angles.
+    public void displayAngles(LinearOpMode context){ // used to display the current angles.
 
-        ctx.telemetry.addData("Current Angle:", robot.getAngle());
-        ctx.telemetry.addData("Forward Angle:", hardware.oldAngle.firstAngle);
-        ctx.telemetry.addData("Correction Angle:", hardware.correction);
-        ctx.telemetry.addData("Current Global Angle:", hardware.globalAngle);
+        context.telemetry.addData("Current Angle:", robot.getAngle());
+        context.telemetry.addData("Forward Angle:", hardware.oldAngle.firstAngle);
+        context.telemetry.addData("Correction Angle:", hardware.correction);
+        context.telemetry.addData("Current Global Angle:", hardware.globalAngle);
 
-        ctx.telemetry.update();
+        context.telemetry.update();
 
     }
 
-    public void displayStatistics(LinearOpMode ctx, double versionName, String type){ // used to display the statistics.
+    public void displayStatistics(LinearOpMode context, double versionName, String type){ // used to display the statistics.
 
-        ctx.telemetry.addData("Action:", "waiting for start");
-        ctx.telemetry.addData("IMU Status: ", hardware.imu.getCalibrationStatus().toString());
-        ctx.telemetry.addData(type + " Version: ", "v" + versionName);
+        context.telemetry.addData("Action:", "waiting for start");
+        context.telemetry.addData("IMU Status: ", hardware.imu.getCalibrationStatus().toString());
+        context.telemetry.addData(type + " Version: ", "v" + versionName);
 
-        ctx.telemetry.addData("Color Sensor Staus: ", hardware.color_sensor_1.toString()); //TODO: IDK what this will retun so test it!
+        context.telemetry.addData("Color Sensor Staus: ", hardware.color_sensor_1.toString()); //TODO: IDK what this will retun so test it!
 
-        ctx.telemetry.update();
+        context.telemetry.update();
 
     }
 

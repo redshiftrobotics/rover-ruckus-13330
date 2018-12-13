8de703fe4f170070caeecbe1c0f6bba987e4d46e
Julian <julian@allchin.com>
2018-12-12 20:47:23 -0800

    Update DistancePowerTester and added sample MR Range Sensor

--- ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DistancePowerTester.java
+++ ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DistancePowerTester.java
@@ -14,6 +14,8 @@ public class DistancePowerTester extends LinearOpMode{
 
     public double timeThreashold = 0;
     public double powerDegrees = 0;
+    public double scale = 1;
+    public boolean pushed = true;
 
     private Robot robot;
     private Hardware hardware;
@@ -27,18 +29,31 @@ public class DistancePowerTester extends LinearOpMode{
         this.robot = new Robot(this.hardware, this);
 
         waitForStart();
-        
+
         while(opModeIsActive()) {
 
             long timer = System.nanoTime();
             int delta_time = (int) ((timer - last_time) / 1000000);
             last_time = timer;
 
-            timeThreashold += gamepad1.left_stick_y * delta_time;
-            powerDegrees += gamepad1.right_stick_y * delta_time;
+            if(gamepad1.dpad_up)
+                scale = 100;
+            else if (gamepad1.dpad_down)
+                scale = 0.1;
+            else if (gamepad1.dpad_left)
+                scale = 10;
+            else if(gamepad1.dpad_right)
+                scale = 1;
+
+            timeThreashold += (gamepad1.left_stick_y * 0.0005 * scale);
+            powerDegrees += (gamepad1.right_stick_y * 0.0005 * scale);
+
+            if(gamepad1.start)
+                powerDegrees = 0.2;
 
             telemetry.addData("time/threashold", timeThreashold);
             telemetry.addData("power/degrees", powerDegrees);
+            telemetry.addData("scale", scale);
             telemetry.update();
 
             if(gamepad1.a){
@@ -46,10 +61,20 @@ public class DistancePowerTester extends LinearOpMode{
             }
 
             if(gamepad1.b){
-                robot.rotate((int) powerDegrees, 1, timeThreashold);
+                robot.rotate((int) powerDegrees, 0.7, timeThreashold);
             }
+
+            if(gamepad1.x)
+                robot.drive(-powerDegrees, timeThreashold);
+
+            if(gamepad1.y)
+                robot.rotate((int) -powerDegrees, 0.7, timeThreashold);
 
             idle();
         }
+    }
+
+    public static double roundToHalf(double d) {
+        return Math.round(d * 4) / 4.0;
     }
 }

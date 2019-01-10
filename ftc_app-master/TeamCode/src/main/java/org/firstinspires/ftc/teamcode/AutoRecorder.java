adaad649be3a8b03caf56e43002a42d0dae1d6bc
LucaSandoval <lucasandoval@seattleacademy.org>
2019-01-09 17:21:11 -0800

    Added an Auto Recorder using AutoMaker.java

---
+++ ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoRecorder.java
@@ -0,0 +1,93 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp(name = "AutoRecorder", group = "13330 Pulsar")
+public class AutoRecorder extends LinearOpMode{
+
+    private AutoMaker autoMaker;
+    private Hardware hardware;
+    private Console console;
+    private boolean isDepo = false;
+
+    private int fileNumber = 0;
+
+    @Override
+    public void runOpMode(){
+
+        Object[][] depoArray = new Object[100][];
+        Object[][] craterArray = new Object[100][];
+
+
+        console.Log("Recorder Initied. Press left/right bumper to set Depo side.", "");
+
+        console.Update();
+
+        while (isDepo == false){
+
+            if(gamepad1.left_bumper){
+                isDepo = true;
+                console.Log("Depo Side is true.", "");
+                console.Update();
+            }
+            if(gamepad1.right_bumper){
+                isDepo = false;
+                console.Log("Depo side is false.", "");
+                console.Update();
+            }
+
+            idle();
+        }
+
+        console.Log("Side selected. Input number for file using D-pad. Press Start to save.", "");
+        console.Update();
+
+        while(!gamepad1.start){
+
+            if (gamepad1.dpad_up){
+                fileNumber = fileNumber + 1;
+
+                console.Log("File Number: ", fileNumber);
+                console.Update();
+            }
+
+            if (gamepad1.dpad_down){
+                fileNumber = fileNumber - 1;
+
+                console.Log("File Number: ", fileNumber);
+                console.Update();
+            }
+
+            idle();
+        }
+
+        console.Log("File number selected.", "");
+        console.Log("Once you have finished, press start to save auto to a file.", "");
+        console.Update();
+
+        waitForStart();
+
+        while (opModeIsActive()){
+
+            if(isDepo == true){
+                autoMaker.record(depoArray, isDepo);
+            } else {
+                autoMaker.record(craterArray, isDepo);
+            }
+
+            if(gamepad1.start){
+                if(isDepo == true){
+                    autoMaker.saveArray(depoArray, "depoAuto");
+                } else {
+                    autoMaker.saveArray(craterArray, "depoAuto");
+                }
+
+            }
+
+            idle();
+
+        }
+
+    }
+}
